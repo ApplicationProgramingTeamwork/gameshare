5b3e701cdc165746d17f47b8ee334f6841884d5c
Pavlov <artem.pavlovs292@gmail.com>
2024-11-18 15:53:39 +0200

    Deleting image from gameform

--- games/forms.py
+++ games/forms.py
@@ -4,7 +4,7 @@ from .models import BoardGame
 class BoardGameForm(forms.ModelForm):
     class Meta:
         model = BoardGame
-        fields = ['name', 'genre', 'description', 'image']
+        fields = ['name', 'genre', 'description']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
         }
